365ee77ea70af85bb78da5f0aa253c50338263ec
khasanovtokhir <khasanovtokhir77@gmail.com>
2022-08-13 09:24:38 +0200

    Faq Section Completed

---
+++ src/Components/FaqSection.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+
+      <div className="question">
+        <h4>Can l learn programming from 0 on your platform?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How much do l have to pay per month?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Is it possible to pay in full in one go?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>If l buy the course, can my brother and l also study?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 // Import Components
 import AboutSection from "../Components/AboutSection";
 import BenefitSection from "../Components/BenefitSection";
+import FaqSection from "../Components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <BenefitSection />
+      <FaqSection />
     </>
   );
 };
